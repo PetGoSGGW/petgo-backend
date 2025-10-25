681fd4e76f90cd0607deff974a81c2c71ecc4e35
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-10-25 21:39:35 +0200

    feat: initial setup

---
+++ src/main/java/pl/petgo/backend/PetGoApplication.java
@@ -0,0 +1,13 @@
+package pl.petgo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetGoApplication.class, args);
+    }
+
+}
