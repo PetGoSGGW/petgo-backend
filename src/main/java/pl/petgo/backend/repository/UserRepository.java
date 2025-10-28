9a26d6fb298e6431086313c31f8a0afffc8c6833
Piotr Gorczyński <p.gorczynski4@gmail.com>
2025-10-28 19:23:30 +0100

    PGB-03 | Added User repository

---
+++ src/main/java/pl/petgo/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package pl.petgo.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.petgo.backend.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
