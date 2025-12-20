471056481b0ab73735b2e9362c9c56bfce3b0f67
Adrian Rostek <adrian.jan.rostek@gmail.com>
2025-12-20 14:33:34 +0100

    [PGB-25] TEMPORARILY disable jwt security

--- src/main/java/pl/petgo/backend/security/SecurityConfig.java
+++ src/main/java/pl/petgo/backend/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                            "/**",
                             "/swagger-ui/**",
                             "/v3/api-docs/**",
                             "/api/auth/register",
