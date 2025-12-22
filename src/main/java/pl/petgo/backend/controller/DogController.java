e1c4a228c85fb7a97de32fe8977e01bf9218ad50
BMarszal <barteq7575@gmail.com>
2025-12-22 17:06:31 +0100

    PGB-09 add swagger operation descriptions for dog module

--- src/main/java/pl/petgo/backend/controller/DogController.java
+++ src/main/java/pl/petgo/backend/controller/DogController.java
@@ -26,52 +26,81 @@ import static org.springframework.http.ResponseEntity.*;
 @RequiredArgsConstructor
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 @Slf4j
-@Tag(name = "Dog Module", description = "Endpoints for managing dogs and their photos.")
+@Tag(name = "Dog Module", description = "Endpoints for managing dogs and their photos")
 public class DogController {
 
     DogService dogService;
 
+    @Operation(
+            summary = "Get all dogs",
+            description = "Fetches a list of all dogs"
+    )
     @GetMapping
     public ResponseEntity<List<DogDto>> getDogs() {
         return ok(dogService.getAllDogs());
     }
 
+    @Operation(
+            summary = "Get a dog by ID",
+            description = "Returns dog information based on the id"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<DogDto> getDog(@PathVariable Long id) {
         return ok(dogService.getDogById(id));
     }
 
-    @Operation(summary = "Add a new dog")
+    @Operation(
+            summary = "Add a new dog",
+            description = "Adds a new dog to the system"
+    )
     @PostMapping
     public ResponseEntity<DogDto> addDog(@RequestBody @Valid DogCreateRequestDto request) {
         return status(CREATED)
                 .body(dogService.addDog(request));
     }
 
-    @Operation(summary = "Update a dog")
+    @Operation(
+            summary = "Update dog details",
+            description = "Updates specific information for an existing dog"
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<DogDto> updateDog(@PathVariable Long id,
                                             @RequestBody @Valid DogUpdateRequestDto request) {
         return ok(dogService.updateDog(id, request));
     }
 
+    @Operation(
+            summary = "Delete a dog",
+            description = "Removes a dog data"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDog(@PathVariable Long id) {
         dogService.deleteDog(id);
         return noContent().build();
     }
 
+    @Operation(
+            summary = "Get dogs by owner",
+            description = "Fetches a list of dogs belonging to a specific owner"
+    )
     @GetMapping(params = "ownerId")
     public ResponseEntity<List<DogDto>> getOwnerDogs(@RequestParam Long ownerId) {
         return ok(dogService.getDogsByOwner(ownerId));
     }
 
+    @Operation(
+            summary = "Get dog photos",
+            description = "Returns a list of photo metadata associated with a specific dog"
+    )
     @GetMapping("/{id}/photos")
     public ResponseEntity<List<DogPhotoDto>> getDogPhotos(@PathVariable Long id) {
         return ok(dogService.getDogPhotos(id));
     }
 
-    @Operation(summary = "Add a photo to a dog")
+    @Operation(
+            summary = "Add photos for a dog",
+            description = "Adds one or more image files and associate them with dog by dog Id"
+    )
     @PostMapping("/{id}/photos")
     public ResponseEntity<List<DogPhotoDto>> addDogPhotos(@PathVariable Long id,
                                                           @RequestPart("files") List<MultipartFile> files) {
@@ -79,6 +108,10 @@ public class DogController {
                 .body(dogService.addDogPhotos(id, files));
     }
 
+    @Operation(
+            summary = "Delete a specific photo",
+            description = "Deletes a specific photo associated with a dog based on the dog Id and photo Id"
+    )
     @DeleteMapping("/{id}/photos/{photoId}")
     public ResponseEntity<Void> deleteDogPhoto(@PathVariable Long id, @PathVariable Long photoId) {
         dogService.deleteDogPhoto(id, photoId);
