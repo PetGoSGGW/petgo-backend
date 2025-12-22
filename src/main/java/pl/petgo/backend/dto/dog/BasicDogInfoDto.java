5ec733c3995137ab947c84e0cd46c4355f32cfa1
Mateusz Boczek <mateusz1_boczek1@interia.pl>
2025-12-22 21:40:42 +0100

    [PGB-13] Renamed field in BasicDogInfoDto

--- src/main/java/pl/petgo/backend/dto/dog/BasicDogInfoDto.java
+++ src/main/java/pl/petgo/backend/dto/dog/BasicDogInfoDto.java
@@ -7,19 +7,19 @@ import pl.petgo.backend.dto.user.BasicUserInfoDto;
 public record BasicDogInfoDto(Long dogId,
                               String name,
                               BreedDto breed,
-                              BasicUserInfoDto userInfoDto) {
+                              BasicUserInfoDto ownerInfoDto) {
 
 
     public static BasicDogInfoDto from(Dog dog) {
         BreedDto breedDto = new BreedDto(dog.getBreed().getBreedCode(), dog.getBreed().getName());
 
         User owner = dog.getOwner();
-        BasicUserInfoDto userInfoDto = BasicUserInfoDto.from(owner);
+        BasicUserInfoDto ownerInfoDto = BasicUserInfoDto.from(owner);
 
         return new BasicDogInfoDto(
                 dog.getDogId(),
                 dog.getName(),
                 breedDto,
-                userInfoDto);
+                ownerInfoDto);
     }
 }
