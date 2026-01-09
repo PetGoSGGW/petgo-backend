22e3a725c63cd63c4dc8a1c7290c13e800333d8b
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2026-01-09 19:12:38 +0100

    fix: update searchOffers + OfferRepository
    Add handling for null lat and lon.
    Utilize spring repository method naming instead of @Query.

--- src/main/java/pl/petgo/backend/repository/OfferRepository.java
+++ src/main/java/pl/petgo/backend/repository/OfferRepository.java
@@ -3,8 +3,6 @@ package pl.petgo.backend.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import pl.petgo.backend.domain.Offer;
 import java.util.Optional;
 
@@ -12,15 +10,9 @@ public interface OfferRepository extends JpaRepository<Offer, Long> {
 
     Optional<Offer> findByWalker_UserId(Long userId);
 
-    @Query("SELECT DISTINCT o FROM Offer o " +
-            "JOIN o.availabilitySlots s " +
-            "WHERE o.isActive = true " +
-            "AND s.reservation IS NULL " +
-            "AND (6371 * acos(cos(radians(:lat)) * cos(radians(s.latitude)) * cos(radians(s.longitude) - radians(:lon)) + sin(radians(:lat)) * sin(radians(s.latitude)))) < :radiusKm")
-    Page<Offer> findAllActiveInRadius(
-            @Param("lat") Double lat,
-            @Param("lon") Double lon,
-            @Param("radiusKm") Double radiusKm,
+    Page<Offer> findDistinctByIsActiveTrueAndAvailabilitySlots_ReservationIsNullAndAvailabilitySlots_LatitudeBetweenAndAvailabilitySlots_LongitudeBetween(
+            Double minLat, Double maxLat,
+            Double minLon, Double maxLon,
             Pageable pageable
     );
 
