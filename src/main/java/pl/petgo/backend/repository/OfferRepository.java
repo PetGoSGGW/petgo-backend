f7af468059fabd374984a482986a48cbadfe2aaf
lukasz_kapron <lukaskapron@gmail.com>
2026-01-28 19:16:28 +0100

    fix PG_BUG_15

--- src/main/java/pl/petgo/backend/repository/OfferRepository.java
+++ src/main/java/pl/petgo/backend/repository/OfferRepository.java
@@ -1,20 +1,18 @@
-package pl.petgo.backend.repository;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.petgo.backend.domain.Offer;
-import java.util.Optional;
+import java.time.Instant;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
 
     Optional<Offer> findByWalker_UserId(Long userId);
 
-    Page<Offer> findDistinctByIsActiveTrueAndAvailabilitySlots_ReservationIsNullAndAvailabilitySlots_LatitudeBetweenAndAvailabilitySlots_LongitudeBetween(
+    Page<Offer> findDistinctByIsActiveTrueAndAvailabilitySlots_ReservationIsNullAndAvailabilitySlots_EndTimeAfterAndAvailabilitySlots_LatitudeBetweenAndAvailabilitySlots_LongitudeBetween(
+            Instant now,
             Double minLat, Double maxLat,
             Double minLon, Double maxLon,
             Pageable pageable
     );
 
-    Page<Offer> findDistinctByIsActiveTrueAndAvailabilitySlots_ReservationIsNull(Pageable pageable);
+    Page<Offer> findDistinctByIsActiveTrueAndAvailabilitySlots_ReservationIsNullAndAvailabilitySlots_EndTimeAfter(
+            Instant now,
+            Pageable pageable
+    );
 }
