e466f13df506d0e3284b431e2f3f00267ad3b422
lukasz_kapron <lukaskapron@gmail.com>
2026-01-28 19:29:36 +0100

    fix imports

--- src/main/java/pl/petgo/backend/repository/OfferRepository.java
+++ src/main/java/pl/petgo/backend/repository/OfferRepository.java
@@ -1,3 +1,10 @@
+package pl.petgo.backend.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.petgo.backend.domain.Offer;
+import java.util.Optional;
 import java.time.Instant;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
